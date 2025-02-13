a81e7fbb68e76db78bfa77cd1f84a1be37af628b
Kshitiz Mhto <kshitizmhto101@gmail.com>
2025-02-13 18:18:42 +0545

    feat: added windows and mac compalibility for setuping the keys

--- cli/setup/setup.go
+++ cli/setup/setup.go
@@ -21,21 +21,18 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
+	"github.com/Kshitiz-Mhto/mock-my-commit/pkg/env"
 	"github.com/Kshitiz-Mhto/mock-my-commit/utility"
 	"github.com/enescakir/emoji"
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
 )
 
-const (
-	APIKEY_STORAGE = ".mock-my-commitrc"
-	HOME           = "HOME"
-)
-
 var (
-	ConfigFile = filepath.Join(os.Getenv(HOME), APIKEY_STORAGE)
+	ConfigFile string
 	SetupCMD   = &cobra.Command{
 		Use:     "setup",
 		Aliases: []string{"set"},
@@ -45,15 +42,57 @@ var (
 	}
 )
 
-func runSetupForTool(cmd *cobra.Command, args []string) {
-	SetupAPIKey()
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		utility.Error("❌ Failed to get home directory: %v", err)
+	}
+
+	switch runtime.GOOS {
+	case env.LINUX_OS:
+		ConfigFile = filepath.Join(homeDir, env.APIKEY_STORAGE)
+	case env.WINDOWS_OS:
+		appDataDir := filepath.Join(homeDir, env.APP_DATA_DIR, env.ROAMING_DIR)
+		ConfigFile = filepath.Join(appDataDir, env.APIKEY_STORAGE)
+
+		if err := os.MkdirAll(appDataDir, 0755); err != nil {
+			utility.Error("\n❌ Failed to create AppData directory: %v", err)
+			os.Exit(1)
+		}
+	default:
+		clickableURL := fmt.Sprintf("\033]8;;%s\033\\%s\033]8;;\033\\", env.GITHUB_REPO_ISSUE_LIST, env.GITHUB_REPO_ISSUE_LIST)
+
+		utility.Error("Unsupported platform/OS. please raise a feature request in our repository [%s]. Thank you!", clickableURL)
+
+		if err := utility.OpenInBrowser(env.GITHUB_REPO_ISSUE_LIST); err != nil {
+			utility.Info("Please manually visit: %s", env.GITHUB_REPO_ISSUE_LIST)
+		}
+	}
 }
 
-func SetupAPIKey() {
+func runSetupForTool(cmd *cobra.Command, args []string) {
+	switch runtime.GOOS {
+	case env.LINUX_OS, env.MAC_OS:
+		SetupAPIKeyForLinuxAndUnix()
+	case env.WINDOWS_OS:
+		SetupAPIKeyForWindows()
+	default:
+		clickableURL := fmt.Sprintf("\033]8;;%s\033\\%s\033]8;;\033\\", env.GITHUB_REPO_ISSUE_LIST, env.GITHUB_REPO_ISSUE_LIST)
+
+		utility.Error("Unsupported platform/OS. please raise a feature request in our repository [%s]. Thank you!", clickableURL)
+
+		if err := utility.OpenInBrowser(env.GITHUB_REPO_ISSUE_LIST); err != nil {
+			utility.Info("Please manually visit: %s", env.GITHUB_REPO_ISSUE_LIST)
+		}
+	}
+}
+
+func SetupAPIKeyForLinuxAndUnix() {
 	fmt.Print("Enter API key: ")
 	key, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
 		utility.Error("\n❌ Failed to read API key: %s", err)
+		os.Exit(1)
 	}
 
 	apiKey := strings.TrimSpace(string(key))
@@ -62,6 +101,42 @@ func SetupAPIKey() {
 		os.Exit(1)
 	}
 
+	if len(apiKey) != 32 {
+		utility.Error("❌ Invalid key format for Mistral API")
+		os.Exit(1)
+	}
+
+	fmt.Println()
+	utility.Info(string(emoji.FileFolder)+"Key saved to %s", ConfigFile)
+	err = os.WriteFile(ConfigFile, []byte(key), 0600)
+	if err != nil {
+		utility.Error("❌ Error saving API key: %s", err)
+		os.Exit(1)
+	}
+
+	utility.Success("🔑 API key saved successfully!!")
+}
+
+func SetupAPIKeyForWindows() {
+	fmt.Print("Enter API key: ")
+	key, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		utility.Error("\n❌ Failed to read API key: %s", err)
+		os.Exit(1)
+	}
+
+	apiKey := strings.TrimSpace(string(key))
+	if apiKey == "" {
+		utility.Error("❌ API key cannot be empty.")
+		os.Exit(1)
+	}
+
+	if len(apiKey) != 32 {
+		utility.Error("❌ Invalid key format for Mistral API")
+		os.Exit(1)
+	}
+
+	fmt.Println()
 	utility.Info(string(emoji.FileFolder)+"Key saved to %s", ConfigFile)
 	err = os.WriteFile(ConfigFile, []byte(key), 0600)
 	if err != nil {
